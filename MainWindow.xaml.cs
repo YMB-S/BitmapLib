fa7a0b4188d1e1a7b7c04621e4eee4c38afb1085
Yorick Bijlsma <yorick.bijlsma@hva.nl>
2024-04-13 22:46:21 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BitmapLib
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly int TARGET_FRAMES_PER_SECOND = 60;
+
+        SimulationDisplay display;
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            SetupSimulationTimer(TARGET_FRAMES_PER_SECOND);
+            InitializeComponent();
+        }
+
+        private void SetupSimulationTimer(int targetFramesPerSecond)
+        {
+            int milliseconds = 1000 / targetFramesPerSecond;
+            timer = new DispatcherTimer()
+            {
+                Interval = new TimeSpan(0, 0, 0, 0, milliseconds)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            display.Update();
+
+            var image = display.GetImage();
+            ImageDisplay.Source = image;
+        }
+
+    }
+}
---
+++ SimulationDisplay.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using FastBitmapLib;
+
+namespace BitmapLib
+{
+    internal class SimulationDisplay
+    {
+        public static readonly int DISPLAY_WIDTH = 100;
+        public static readonly int DISPLAY_HEIGHT = 100;
+
+        Bitmap bitmap;
+
+        public SimulationDisplay()
+        {
+            bitmap = new Bitmap(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+        }
+
+        public BitmapImage GetImage()
+        {
+            DrawTo(bitmap);
+            return ToBitmapImage(bitmap);
+        }
+
+        public void Update()
+        {
+        }
+
+        public void DrawTo(Bitmap target)
+        {
+            using (var bitmap = target.FastLock())
+            {
+                bitmap.Clear(Color.White);
+            }
+        }
+
+        private BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
