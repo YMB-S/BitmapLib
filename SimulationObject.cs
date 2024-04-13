49e961c60ef87de6e3a3c31b36a8d137d83df50d
Yorick Bijlsma <yorick.bijlsma@hva.nl>
2024-04-13 23:20:30 +0200

    Change ISimulationObject to SimulationObject (interface to base class)

---
+++ SimulationObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmapLib
+{
+    public abstract class SimulationObject
+    {
+        private Vector2 position;
+        Vector2 Position { get; }
+
+        private Vector2 velocity;
+        Vector2 Velocity { get; }
+
+        public void Update()
+        {
+            position += velocity;
+        }
+
+        public abstract void Draw(); // return some (x, y, color) struct-pixels here
+
+    }
+}
